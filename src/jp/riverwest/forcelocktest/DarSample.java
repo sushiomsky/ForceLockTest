aa59605cceec3de5dd47c88dc3ab8138bf81407a
Hiroki Kawanishi <hiroki.kawanishi1106@gmail.com>
2014-06-17 16:54:17 +0900

    add DevicePolicyManager

---
+++ src/jp/riverwest/forcelocktest/DarSample.java
@@ -0,0 +1,19 @@
+package jp.riverwest.forcelocktest;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DarSample extends DeviceAdminReceiver {
+
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        super.onEnabled(context, intent);
+    }
+
+    @Override
+    public void onDisabled(Context context, Intent intent) {
+        super.onDisabled(context, intent);
+    }
+
+}
--- src/jp/riverwest/forcelocktest/MainActivity.java
+++ src/jp/riverwest/forcelocktest/MainActivity.java
@@ -1,18 +1,25 @@
 package jp.riverwest.forcelocktest;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
+    static final int RESULT_ENABLE = 1;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,13 @@ public class MainActivity extends ActionBarActivity {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
         }
+        
+        // ComponentNameを取得する
+        ComponentName darSample = new ComponentName(this, DarSample.class);
+        // デバイス管理を有効にする
+        Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, darSample);
+        startActivityForResult(intent, RESULT_ENABLE);
     }
 
     @Override
@@ -48,14 +62,53 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        DevicePolicyManager mDevicePolicyManager;
+        ComponentName mDarSample;
+
         public PlaceholderFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            
+            Activity a = getActivity();
+            Context context = a.getApplicationContext();
+            
+            // DevicePolicyManagerを取得する
+            mDevicePolicyManager = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+
+            Button lockButton = (Button) rootView.findViewById(R.id.lock_button);
+            lockButton.setOnClickListener(new View.OnClickListener() {
+                
+                @Override
+                public void onClick(View v) {
+                    
+                    // 画面ロック
+                    mDevicePolicyManager.lockNow();
+                }
+            });
+
             return rootView;
         }
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        
+        switch (requestCode) {
+        case RESULT_ENABLE:
+            if (resultCode == Activity.RESULT_OK) {
+                Log.i("DeviceAdminSample", "Administration enabled!");
+            }
+            else {
+                Log.i("DeviceAdminSample", "Administration enable FAILED!");
+            }
+            
+            return;
+        }
+        
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 }
